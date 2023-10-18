ed60c5aa8f4d6ee1a81705f1bdaea05e518a8b5d
Kimberly Koh <13kkoh@gmail.com>
2023-10-18 00:00:29 -0500

    trying new print statements

--- server/api/index.js
+++ server/api/index.js
@@ -21,8 +21,10 @@ app.use((req, rest, next) => {
 
 app.use("/api/:collection", (req, res, next) => {
     if (req.params.collection === "inventory") {
+        console.log("inventory route hit")
         inventoryRoutes(req, res, next); 
     } if (req.params.collection === "transaction") {
+        console.log("transction route hit")
         transactionRoutes(req, res, next); 
     } else if (req.params.collection === "reservation"){
         reservationRoutes(req, res, next); 
@@ -32,11 +34,12 @@ app.use("/api/:collection", (req, res, next) => {
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen((process.env.PORT), () => {
-            console.log(`The server is listening on port ${process.env.PORT}`)
+            console.log(`Kim's server is listening on port ${process.env.PORT}`)
         });     
     }) 
     .catch((err) => {
-        console.log(err); 
+        console.log(err);
+        res.status(400).json({error: err.message})
     })
 
 //exporting express API
