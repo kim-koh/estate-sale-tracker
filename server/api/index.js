b63045a20b5971dd05039606bb31c10f3e635088
Kimberly Koh <13kkoh@gmail.com>
2023-12-14 15:18:58 -0600

    updating header

--- server/api/index.js
+++ server/api/index.js
@@ -20,7 +20,7 @@ app.use((req, rest, next) => {
 }); 
 
 app.use("/api/:collection", (req, res, next) => {
-    res.setHeader('https://estate-sale-tracker-frontend.vercel.app');
+    res.setHeader("Access-Control-Allow-Origin", 'https://estate-sale-tracker-frontend.vercel.app');
     //OR for testing
     //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
